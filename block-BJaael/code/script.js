91b782ff380778b5371ed6b67f6797e8e0180936
PujaGKundu <pujakundu100@gmail.com>
2021-11-17 12:06:08 +0530

    Ass: Dosco Grid

--- block-BJaael/code/script.js
+++ block-BJaael/code/script.js
@@ -1,5 +1,8 @@
+function randomNumber(max) {
+    return (Math.round(Math.random() * max));
+}
 
-function get_random_color() {
+function random_color() {
     var letters = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'];
     var color = '#';
     for (var i = 0; i < 6; i++ ) {
@@ -9,27 +12,9 @@ function get_random_color() {
     return color;
 }
 
-
-function randomNumber(max) {
-    return (Math.round(Math.random() * max));
-}
-
-
-
-let allboxes = document.querySelectorAll('.box');
-
-function handleMouseMove() {
-    allboxes.forEach(box => {
-        box.style.backgroundColor = get_random_color;
-        box.querySelector('h3').innerText = randomNumber(500);
-    });
-}
-
-
 let parentBox = document.querySelector(".boxes")
 
-
-for(let i = 0; i < 500; i++) {
+for(let i = 0; i < 500; i++) { 
     let div = document.createElement('div');
     div.classList.add('box');
     let h3 = document.createElement('h3');
@@ -39,6 +24,22 @@ for(let i = 0; i < 500; i++) {
     parentBox.append(div);
 }
 
+let allboxes = document.querySelectorAll('.box');
 
 parentBox.addEventListener('mousemove', handleMouseMove);
+
+function handleMouseMove() {
+    allboxes.forEach((box) => {
+        box.style.backgroundColor = random_color();
+        box.querySelector('h3').innerText = randomNumber(500);
+    });
+}
+
+
+
+
+
+
+
+
 
