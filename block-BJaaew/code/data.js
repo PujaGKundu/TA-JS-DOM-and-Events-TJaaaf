8f6fc8daa39bdf5ad9c70e7a4b07a85473943aaf
PujaGKundu <pujakundu100@gmail.com>
2021-11-24 09:52:00 +0530

    People of GOT

--- block-BJaaew/code/data.js
+++ block-BJaaew/code/data.js
@@ -326,3 +326,59 @@ let got = {
     },
   ],
 };
+
+
+
+
+
+
+let div = document.querySelector(".familyName");
+
+
+let ul = document.querySelector('ul');
+
+
+
+
+let allPeople = got.houses.reduce((acc, cv) => {
+  acc = acc.concat(cv.people);
+  return acc;
+}, []);
+
+allPeople.forEach(peoples => {
+    let li = document.createElement('li');
+    li.classList.add('box');
+    let div = document.createElement('div');
+    div.classList.add('flex');
+    let img = document.createElement('img');
+    img.src = peoples.image;
+    img.alt = peoples.name;
+    let h2 = document.createElement('h2');
+    h2.innerText = peoples.name;
+    let p = document.createElement('p');
+    p.innerText = peoples.description;
+    div.append();
+    let button = document.createElement('button');
+    let a = document.createElement('a');
+    a.innerText = "Know More!"
+    a.href = peoples.wikiLink;
+    button.append(a);
+    li.append(img, h2, p, button);
+    ul.append(li);
+});
+
+function handleFamilyClick(event) {
+  console.log(event);
+}
+
+got.houses.forEach((names, index) => {
+  let button = document.createElement('button');
+  button.innerText = names.name;
+  button.setAttribute("data-id", index);
+  div.append(button);
+  button.addEventListener('click', handleFamilyClick(button));
+});
+
+
+
+
